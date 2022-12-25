dc736270ebf329cf738c3c616aabd5a9394453c2
Patricia Ruhstrat <32817506+ruhstratp@users.noreply.github.com>
2022-12-25 23:48:48 +0200

    Update AllBooks.svelte

--- src/Components/AllBooks.svelte
+++ src/Components/AllBooks.svelte
@@ -1,201 +1,196 @@
 <script>
-    import BookCard from "./Subcomponents/BookCard.svelte";
 </script>
 
-<!--<BookCard bookId={2} title={"In jurul lumii in 3 zile"} author={"Gigel"} /> -->
 <body>
-    <div id="large-th">
-        <div class="container">
-            <div id="list-th">
-                <a style="color:black;" href="#/book_details/:id">
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg" alt='Book 1'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            Frankenstein<br />
-                            <span class="author">Mary Shelley</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="https://alysbcohen.files.wordpress.com/2015/01/little-princess-book-cover.jpg" alt='Book 2'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            A Little Princess<br />
-                            <span class="author">Frances Hodgson Burnett</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="http://www.publishersweekly.com/images/data/ARTICLE_PHOTO/photo/000/028/28129-1.JPG" alt='Book 3'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            Roughing It<br />
-                            <span class="author">Mark Twain</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="http://talkingwriting.com//sites/default/files/Bird-by-Bird-image1.jpg" alt='Book 4'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            Bird By Bird<br />
-                            <span class="author">Anne Lamott</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="http://d.gr-assets.com/books/1414348859l/23209971.jpg" alt='Book 5'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            Girl at War<br />
-                            <span class="author">Sara Novic</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="http://prodimage.images-bn.com/pimages/9780062315007_p0_v2_s192x300.jpg" alt='Book 6'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            The Alchemist<br />
-                            <span class="author">Paulo Coelho</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="http://eastchapelhillobserver.com/wp-content/uploads/2015/02/amazondotcom.jpg" alt='Book 7'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            Anne of Green Gables<br />
-                            <span class="author">Lucy Maud Montgomery</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a style="color:black;" href="#/book_details/:id" >
-                <div class="book">
-                    <div class="cover">
-                        <img
-                            src="http://www.penguinbooksindia.com/sites/default/files/book_image/9780143416319.jpg" alt='Book 8'
-                        />
-                    </div>
-                    <div class="description">
-                        <p class="title">
-                            Animal Farm<br />
-                            <span class="author">George Orwell</span>
-                        </p>
-                    </div>
-                </div>
-            </a>
-            </div>
+  <header>
+    <div class="flex-row">
+      <img class="logo" src="\src\img\logo@1x.jpg" alt="logo" />
+      <h1 style="font-size:3.5vw;font-weight:bold">BookStars</h1>
+
+      <nav>
+        <div class="overlap-group8">
+          <div class="searchBar">
+            <input
+              type="text"
+              class="searchTerm"
+              placeholder="Search book..."
+            />
+          </div>
+          <ul class="navbar">
+            <li class="nav-item"><a class="nav-link" href="#/">Home</a></li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/reviews">Reviews</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/all_books">Books</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/forum">Forum</a>
+            </li>
+            <li>
+              <button type="submit" class="btn btn-link btn-logout"
+                >Logout</button
+              >
+            </li>
+          </ul>
         </div>
+      </nav>
     </div>
+  </header>
+  <!-- <img
+  class="pexels-bg"
+  src="\src\img\pexels-cottonbro-4861347-1@1x.png"
+  alt="pexels-cottonbro-4861347 1"
+/>
+  <h1 class="the-place-where-you valign-text-middle">
+    The place where you don't only find new books, but also new opinions ...
+  </h1> -->
 </body>
 
 <style>
-    .container {
-        margin: 0 auto;
-        max-width: 910px;
-    }
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
+  @import url("https://fonts.googleapis.com/css?family=Poppins:200,400,600|Bitter:400,600italic,600|Playfair+Display:700");
+  .flex-row {
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 90px;
+    display: flex;
+    align-items: center;
+    box-shadow: 0 0 10px 0 black;
+    margin-bottom:-10px;
+    z-index: 2;
+    width: 100%;
+    
+  }
+  body {
+    font-family: Bitter;
+  }
 
-    .book {
-        transition:transform 0.25s ease;
-        display: inline-block;
-        width: 230px;
-        height: 390px;
-        box-shadow: 0 0 20px #aaa;
-        margin: 25px;
-        padding: 10px 10px 0 10px;
-        vertical-align: top;
-        transition: height 1s;
-    }
-    /* star button */
-    .book:after {
-        font-family: FontAwesome;
-        content: "\f006";
-        font-size: 35px;
-        position: relative;
-        left: -0.1cm;
-        top: -0.8cm;
-        float: right;
-    }
-    .book:hover{
-    -webkit-transform:scale(0.5); 
-    transform:scale(1.1);
-}
 
-    .cover {
-        border: 2px solid gray;
-        height: 80%;
-        overflow: hidden;
-    }
+  header {
+    background-color: white;
+    position: top;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: auto;
+    display: flex;
+    align-items: center;
+  }
 
-    .cover img {
-        width: 100%;
-    }
+  header * {
+    display: inline;
+  }
 
-    .book p {
-        margin-top: 12px;
-        font-size: 20px;
-    }
+  header li {
+    margin: 20px;
+  }
 
-    .book .author {
-        font-size: 15px;
-    }
-    @media (max-width: 941px) {
-        .container {
-            max-width: 700px;
-        }
-        .book {
-            margin: 49px;
-        }
-    }
-    @media (max-width: 730px) {
-        .book {
-            display: block;
-            margin: 0 auto;
-            margin-top: 50px;
-        }
-    }
+  header li a {
+    color: black;
+    text-decoration: none;
+    margin-right: -30px;
+  }
 
+
+  .overlap-group8 {
+    /* background-color: beige; */
+    border-radius: 30px;
+    display: flex;
+    height: 90px;
+    justify-content: flex-start;
+    margin-left: 120px;
+    width: 900px;
+    /* min-width: 700px; */
+    padding: 31px 20px;
+  }
+  .btn {
+    color: black;
+  }
+  .btn:hover {
+    font-weight: bold;
+    color: rgb(95, 48, 0);
+  }
+  .searchTerm {
+    width: 100%;
+    border-right: none;
+    padding: 5px;
+    /* height: 20px; */
+    outline: none;
+  }
+
+  .searchTerm:focus {
+    color: rgb(95, 48, 0);
+  }
+
+  .searchTerm:hover {
+    font-style: italic;
+  }
+
+  .searchBar input[type="text"] {
+    background: none;
+    float: left;
+    padding: 6 6 6 0;
+    border: none;
+    margin-top: none;
+    margin-right: 16px;
+    font-size: 17px;
+    /* width: 250px; */
+  }
+
+  .nav-link {
+    color: black;
+  }
+
+  .nav-link:hover {
+    font-weight: bold;
+    font-style: italic;
+  }
+
+  .navbar {
+    margin-bottom: 50px;
+    background: white;
+    width: 700px;
+    padding-left: 30px;
+    padding-top: 0 !important;
+  }
+
+  .searchBar {
+    /* width: 100%; */
+    position: relative;
+    display: flex;
+    outline: auto;
+    outline-color: rgb(243, 228, 160);
+    border-radius: 30px;
+    height: 55px;
+    /* min-width: 200px; */
+    padding: 20px 58px;
+    margin: -10px 0 0 10px;
+  }
+
+  .logo {
+    margin-left: 20px;
+    height: 85px;
+    margin-bottom: 5px;
+    object-fit: cover;
+    width: 78px;
+  }
+
+  @media screen and (max-width: 600px) {
+    .navbar a,
+    .searchBar input[type="text"] {
+      float: none;
+      display: block;
+      text-align: left;
+      width: 100%;
+      margin: 0;
+      padding: 14px;
+    }
+    .searchBar input[type="text"] {
+      border: 1px solid #ccc;
+    }
+  }
 </style>
